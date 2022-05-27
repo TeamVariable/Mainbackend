7eff67b9537e3bbd3d65fa607c571fe10ff0082e
Joe <at365@naver.com>
2022-05-28 01:34:15 +0900

    Start Project

---
+++ src/main/java/com/backend/rootbackend/RootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.rootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/rootbackend/RootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.rootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
