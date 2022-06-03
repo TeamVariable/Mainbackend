527dea33bed0fdd93a9f9d224711a8d9b2e3a0e0
Joe <at365@naver.com>
2022-06-03 21:26:31 +0900

    feat:Update User Repository

--- backend/src/main/java/com/backend/rootbackend/domain/repository/UserRepository.java
+++ backend/src/main/java/com/backend/rootbackend/domain/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.backend.rootbackend.domain.repository;
 
-public interface UserRepository {
+import com.backend.rootbackend.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>, QuerydslPredicateExecutor<User> {
 }
