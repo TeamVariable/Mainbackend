1666f19e0d3fb62488ae182dae5c8df098db6735
Joe <at365@naver.com>
2022-06-03 21:26:19 +0900

    feat:Update User entity

--- backend/src/main/java/com/backend/rootbackend/domain/entity/User.java
+++ backend/src/main/java/com/backend/rootbackend/domain/entity/User.java
@@ -1,4 +1,36 @@
 package com.backend.rootbackend.domain.entity;
 
-public class User {
+import com.backend.rootbackend.global.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "uuid2"
+    )
+    private UUID userIdx;
+
+    private String name;
+    private String nickname;
+    private String profileImageUrl;
+    private String phoneNum;
+    private String userEmail;
+    private String userPassword;
+
+
 }
