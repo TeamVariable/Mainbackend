9436bd7aca8630a40bd305f4e25b2b59b54686b3
Joe <at365@naver.com>
2022-06-05 00:14:16 +0900

    feat:Update User entity

--- backend/src/main/java/com/backend/rootbackend/domain/entity/User.java
+++ backend/src/main/java/com/backend/rootbackend/domain/entity/User.java
@@ -1,17 +1,16 @@
 package com.backend.rootbackend.domain.entity;
 
-import com.backend.rootbackend.global.BaseEntity;
+import com.backend.rootbackend.global.common.BaseEntity;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Table(name = "Users")
 @Getter
 @Setter
 @Builder
@@ -26,11 +25,27 @@ public class User extends BaseEntity {
     private UUID userIdx;
 
     private String name;
+
     private String nickname;
+
+    @Column(name = "profile_image_url")
     private String profileImageUrl;
+
+    @Column(name = "phone_num")
     private String phoneNum;
+
+    @Column(name = "user_email")
     private String userEmail;
+
+    @Column(name = "user_password")
     private String userPassword;
 
+
+    public void modifyUser(String nickname, String password, String phoneNum, String profileImageUrl) {
+        this.nickname = nickname;
+        this.userPassword = password;
+        this.phoneNum = phoneNum;
+        this.profileImageUrl = profileImageUrl;
+    }
 
 }
